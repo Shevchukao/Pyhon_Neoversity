c2ea2277d4eca31b42857a16727ac7faaf34d380
Anton Shevchuk <anton.shevchuk.1998@gmail.com>
2025-10-24 19:31:31 +0300

    1

---
+++ module02/for.py
@@ -0,0 +1,30 @@
+for i in range(10):
+    print(i)
+
+
+students = ["Anton", "Vasya"]
+
+for name in students:
+    print(name)
+
+for letter in "Anton Shevchuk":
+    print(letter)
+
+
+numbers = [1, 2, 47, 89, 14, -2, -25, 87, 65]
+
+numbers.sort() #
+
+min_number = numbers[0]
+
+numbers.sort(reverse=True)
+
+max_number = numbers[0]
+
+print(f"Min number in list: {min_number}, max number in list: {max_number}")
+
+
+
+
+
+

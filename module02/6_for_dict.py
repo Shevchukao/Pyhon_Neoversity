b80aee7c175d90214a87360dd5e2d3a2bc3d6c1a
Anton Shevchuk <anton.shevchuk.1998@gmail.com>
2025-10-28 00:41:11 +0200

    dict

--- module02/6_for_dict.py
+++ module02/6_for_dict.py
@@ -1,4 +1,6 @@
+
 dct = {1: "Anton", 2: "Andrey", 3: "Peter", 4: "Alex"}
-for i in range(len(dct)):
-    
-    print(dct)
+# [for ... in ...] construction need for iterable over a sequencence of elements(such as string, list, dictionary ,tuples, ranges)
+for key in dct:
+    dct[key] = dct[key]
+    print(dct[key])
